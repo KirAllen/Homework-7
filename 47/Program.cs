21ddb580b80baabfc6e71dffcbdbb5eda1f76b7b
Kirill Allyanov <kir.allyanov@gmail.com>
2022-09-24 17:55:26 +0300

    Other Tasks Added

---
+++ 47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Enter Rows");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Columns");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[rows, columns]; 
+CreateArray(array); 
+PrintArray(array); 
+
+void CreateArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().NextDouble() * 100; 
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");  
+        }
+     Console.WriteLine();
+    }
+    
+}
+
---
+++ 53/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Enter Rows");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Columns");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns]; 
+CreateArray(array); 
+PrintArray(array); 
+GetArithmeticMean(array);
+
+void CreateArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,9); 
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");  
+        }
+     Console.WriteLine();
+    }
+    
+}
+
+void GetArithmeticMean(int[,] array)
+{   
+    
+    for(int j = 0; j < array.GetLength(1); j++)
+    {   
+        float sum = 0;
+        float mean = 0; 
+
+        for(int i = 0; i < array.GetLength(0); i++)
+        {   
+             sum = sum + array[i,j];
+             mean = sum / array.GetLength(1); 
+        }  
+        Console.WriteLine($"Arithmetic Mean {j} = {mean}");
+    } 
+}
+
